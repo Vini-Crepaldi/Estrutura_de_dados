54a3757ad1c33d5cc7e7b42e861f768a5b1b821f
Crepaldi <108742768+Vini-Crepaldi@users.noreply.github.com>
2025-05-27 23:03:51 -0300

    Create Ex_01.py

---
+++ Ex_01.py
@@ -0,0 +1,8 @@
+#valor fixo
+
+salario_fixo = 500.00
+vendas = 12398.00
+comissao = vendas * 0.06
+faturamento_total = salario_fixo + comissao
+
+print(f"Faturamento total: R$ {faturamento_total:.2f}")
